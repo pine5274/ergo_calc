65a07d76a7166fc415e9c9663833bef19104abf5
sou-komatsu <sou-komastu@mahoujin.co.jp>
2023-11-14 13:39:43 +0900

    commit

--- py/function.py
+++ py/function.py
@@ -9,10 +9,20 @@ def sec_to_time(time):
         return str(m) + ':0' + str(s)
     return str(m) + ':' + str(s)
 
+# 怪しい
+def time_to_sec(x):
+    if (type(x) is float):
+        return x
+    minutes = int(x[3])
+    seconds = int(x[-2:])
+    return minutes * 60 + seconds
+
 def watt_to_pace(watt):
-    pace = 500 * (2.80 / watt) ** (1/3)
+    sec = 500 * (2.80 / watt) ** (1/3)
+    pace = sec_to_time(sec)
     return pace
 
 def pace_to_watt(pace):
-    watt = 2.80 / (pace / 500) ** 3
+    sec = time_to_sec(pace)
+    watt = 2.80 / (sec / 500) ** 3
     return watt
